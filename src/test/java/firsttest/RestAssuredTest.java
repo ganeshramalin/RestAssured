bff966c82927db3a8ee5d3c8d42a8fef7dd675c2
Aditya Parab <adiparab14@gmail.com>
2017-11-15 10:05:47 +0100

    Creating Project

---
+++ src/test/java/firsttest/RestAssuredTest.java
@@ -0,0 +1,24 @@
+package firsttest;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class RestAssuredTest {
+	
+	
+	@Test
+	public void checkHttpStatus() {
+		
+		given().
+		when().
+			get("https://ergast.com/api/f1/2008/").
+		then().
+			assertThat().
+			statusCode(200);
+		
+		
+	}
+}
