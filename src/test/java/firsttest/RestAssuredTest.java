afc3308452fb3ee91630b54846c6740f8e5e29ce
ganeshramalin <41120661+ganeshramalin@users.noreply.github.com>
2018-08-28 15:34:36 +0200

    Update RestAssuredTest.java

--- src/test/java/firsttest/RestAssuredTest.java
+++ src/test/java/firsttest/RestAssuredTest.java
@@ -8,19 +8,28 @@ import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTest {
 	
-	
 	@Test
-	public void checkHttpStatus() {
-		
-		given().
-		when().
-			get("http://ergast.com/api/f1/2017/circuits.json").
-		then().
-			assertThat().
+	public void test_ScenarioRetrieveFirstCircuitFor2017SeasonAndGetCountry_ShouldBeAustralia() {
+        
+	    // First, retrieve the circuit ID for the first circuit of the 2017 season
+	    String circuitId = given().
+	    when().
+		get("http://ergast.com/api/f1/2017/circuits.json").
+	    then().
+		assertThat().
 			statusCode(200);
 		and().
-       			 contentType(ContentType.JSON).
-   		and().
-        		header("Content-Length",equalTo("4567"));			
+       			contentType(ContentType.JSON).
+		extract().
+		path("MRData.CircuitTable.Circuits.circuitId[0]");
+	    // Then, retrieve the information known for that circuit and verify it is located in Australia
+	    given().
+		pathParam("circuitId",circuitId).
+	    when().
+		get("http://ergast.com/api/f1/circuits/{circuitId}.json").
+	    then().
+		assertThat().
+		body("MRData.CircuitTable.Circuits.Location[0].country",equalTo("Australia"));
 	}
+	
 }
